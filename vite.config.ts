63caa17565390da6dc27ea594e051b2c1f8373c0
MathieuLemoyne <159171247+MathieuLemoyne@users.noreply.github.com>
2025-02-18 09:43:37 -0500

    edit

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
-base: "/appweb-atel08/";
 import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/appweb-atel08/",
   plugins: [vue()],
 });
