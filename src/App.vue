d409f457da07ac476172bce66253f067951b89aa
MathieuLemoyne <159171247+MathieuLemoyne@users.noreply.github.com>
2025-02-18 09:38:50 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Timeline from "./components/Timeline.vue";
+import Header from './components/Header.vue'
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+</script>
+
+<template>
+  <Header logo="/logo.png" title="Ma page d'accueil"/>
+  <Timeline />
+</template>
+
+<style></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{
+    logo: string
+    title: string
+}>()
+</script>
+
+<template>
+  <header>
+    <img :src="logo" alt="Logo">
+    <h1>{{ title }}</h1>
+  </header>
+</template>
